fd3f3191f800b7be006bc99d3a1bd70afaed2aa8
Jeremie Dimino <jeremie@dimino.org>
2017-11-11 10:24:39 +0000

    fixes

--- bin/cli.ml
+++ bin/cli.ml
@@ -1,24 +1,23 @@
 open Cmdliner
 
 let fact =
-  ( Term.(const Math.fact
+  ( Term.(const (fun n -> Printf.printf "%d\n" (Math.fact n))
           $ Arg.(required & pos 0 (some int) None & Arg.info [] ~docv:"N"))
   , Term.info "fact"
       ~doc:"Compute and print the factorial of a number"
       ~man:[ `S "DESCRIPTION"
            ; `P {|$(b,math fact N) computes and print the factorial of $(b,N)|}
-           ; `Blocks help_secs
            ]
   )
 
 let gcd =
-  ( Term.(const Math.fact
-          $ Arg.(required & pos 0 (some int) None & Arg.info [] ~docv:"N"))
+  ( Term.(const (fun a b -> Printf.printf "%d\n" (Math.gcd a b))
+          $ Arg.(required & pos 0 (some int) None & Arg.info [] ~docv:"A")
+          $ Arg.(required & pos 1 (some int) None & Arg.info [] ~docv:"B"))
   , Term.info "gcd"
       ~doc:"Compute and print the greatest common divisor of two numbers"
       ~man:[ `S "DESCRIPTION"
            ; `P {|$(b,math gcd N) computes and print the factorial of $(b,N)|}
-           ; `Blocks help_secs
            ]
   )
 
@@ -26,7 +25,7 @@ let all = [fact; gcd]
 
 let default =
   let doc = "Math utilities" in
-  ( Term.(ret (const (fun _ -> `Help (`Pager, None)) $ common))
+  ( Term.(ret (const (`Help (`Pager, None))))
   , Term.info "math" ~doc
       ~man:
         [ `S "DESCRIPTION"
