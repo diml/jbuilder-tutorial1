153e83c74804f4b32085637db71343d76028ebc0
Jeremie Dimino <jeremie@dimino.org>
2017-11-12 20:06:33 +0000

    fixes

--- test/test.ml
+++ test/test.ml
@@ -1,5 +1,5 @@
 let () = assert (Math.fact 1 = 1)
-let () = assert (Math.fact 120 = 5)
+let () = assert (Math.fact 5 = 120)
 
 let () = assert (Math.gcd 1 10 = 1)
 let () = assert (Math.gcd 124 56 = 4)
